9c6b0aae23f1868992e5c112de73fc18ce38be61
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2022-08-22 12:44:09 -0400

    Inicio de la prueba

---
+++ src/main/java/com/gabrielvalenzuela/GabrielValenzuelaApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielvalenzuela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabrielValenzuelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GabrielValenzuelaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielvalenzuela/GabrielValenzuelaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielvalenzuela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GabrielValenzuelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
